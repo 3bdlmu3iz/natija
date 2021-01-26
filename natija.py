263e9ab19eec44619a5a43ed4f3fdc5ff0d807f9
3bdlmu3iz <76874479+3bdlmu3iz@users.noreply.github.com>
2021-01-26 11:41:58 +0100

    Add files via upload

---
+++ natija.py
@@ -0,0 +1,63 @@
+math1 = float(input("math 1?\n>"))
+math2 = float(input("math 2?\n>"))
+math3 = float(input("math 3?\n>"))
+math4 = float(input("math 4?\n>"))
+math = (math1 + math2 + math3 + math4) / 4
+math = math * 9
+
+pc1 = float(input("pc 1?\n>"))
+pc2 = float(input("pc 2?\n>"))
+pc3 = float(input("pc 3?\n>"))
+pc4 = float(input("pc 4?\n>"))
+pc = (pc1 + pc2 + pc3 + pc4) / 4
+pc = pc * 7
+
+fr1 = float(input("fr 1?\n>"))
+fr2 = float(input("fr 2?\n>"))
+fr3 = float(input("fr 3?\n>"))
+fr4 = float(input("fr 4?\n>"))
+fr5 = float(input("fr 5?\n>"))
+fr = (fr1 + fr2 + fr3 + fr4 + fr5) / 5
+fr = fr * 4
+
+ar1 = float(input("ar 1?\n>"))
+ar2 = float(input("ar 2?\n>"))
+ar3 = float(input("ar 3?\n>"))
+ar = (ar1 + ar2 + ar3) / 3
+ar = ar * 2
+
+eng1 = float(input("eng 1?\n>"))
+eng2 = float(input("eng 2?\n>"))
+eng3 = float(input("eng 3?\n>"))
+eng = (eng1 + eng2 + eng3) / 3
+eng = eng * 2
+
+filo1 = float(input("filo 1?\n>"))
+filo2 = float(input("filo 2?\n>"))
+filo3 = float(input("filo 3?\n>"))
+filo = (filo1 + filo2 + filo3) / 3
+filo = filo * 2
+
+sport1 = float(input("sport 1?\n>"))
+sport2 = float(input("sport 2?\n>"))
+sport3 = float(input("sport 3?\n>"))
+sport = (sport1 + sport2 + sport3) / 3
+sport = sport * 4
+
+svt1 = float(input("svt 1?\n>"))
+svt2 = float(input("svt 2?\n>"))
+svt3 = float(input("svt 3?\n>"))
+svt = (svt1 + svt2 + svt3) / 3
+svt = svt * 3
+
+ei1 = float(input("ei 1?\n>"))
+ei2 = float(input("ei 2?\n>"))
+ei3 = float(input("ei 3?\n>"))
+ei = (ei1 + ei2 + ei3) / 3
+ei = ei * 2
+
+ac = float(input("ASSIDUITE ET CONDUITE ?\n>"))
+
+natija = math + pc + svt + filo  + eng + ar + fr + ei + sport + ac
+natija = natija / 36
+print("jbti", natija,"/20 albatala\nmbrok 3lik hhh")
